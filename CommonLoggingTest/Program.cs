7d1d32e2495e4f7793f2b2f8b707a98357704353
alexzaitzev <mail@alexzaitzev.pro>
2018-06-22 23:03:41 +0300

    Initial commit

---
+++ CommonLoggingTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Common.Logging;
+
+namespace CommonLoggingTest
+{
+    class Program
+    {
+        private static readonly ILog Logger = LogManager.GetLogger<Program>();
+
+        static void Main(string[] args)
+        {
+            SetupLogging();
+            Logger.Debug("Test logging");
+            Console.ReadLine();
+        }
+
+        private static void SetupLogging()
+        {
+            var props = new Common.Logging.Configuration.NameValueCollection
+            {
+                { "configType", "FILE" },
+                { "configFile", "./nlog.config" }
+            };
+
+            LogManager.Adapter = new Common.Logging.NLog.NLogLoggerFactoryAdapter(props);
+        }
+    }
+
+}
